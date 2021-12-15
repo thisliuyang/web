28c3e03d8e86bf5e070d0b9ad657257cede8f8d1
liuyang <liuyang@metadata.net.cn>
2021-12-15 22:56:55 +0800

    feat: 优化响应式

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 window.onscroll = function () {
-  var scrollTop =
-    document.documentElement.scrollTop || document.body.scrollTop;
+  var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
   console.log("滚动距离" + scrollTop);
 
   var scrollingContainer = document.querySelector(
     ".main-horiztonal-scrolling-container"
   );
+
+  var nav = document.querySelector(".nav");
 
   console.log(
     "scrollingContainer.clientHeight :>> ",
@@ -14,20 +15,25 @@ window.onscroll = function () {
   var t = scrollingWrapper.getBoundingClientRect().top;
 
   console.log("t :>> ", t);
+  if (t < 0) {
+    console.log("nav :>> ", nav);
+    nav.style.cssText = "background: rgba(9, 5, 17, 1);";
+  } else {
+    nav.style.cssText = "background: rgba(9, 5, 17, 0.6);";
+  }
 
   var percent = Math.abs(t) / scrollingContainer.clientHeight;
 
   console.log("percent :>> ", percent);
   var r = horizontal.clientWidth * percent;
 
-  if (t == -scrollingContainer.clientHeight * (2/3)) {
+  if (t == -scrollingContainer.clientHeight * (2 / 3)) {
     console.log("ttttt :>> ", t);
   }
 
-  const rAF =
-    window.requestAnimationFrame || ((func) => setTimeout(func, 16));
+  const rAF = window.requestAnimationFrame || ((func) => setTimeout(func, 16));
   const frameFunc = () => {
-    if (t < 0 && t > -scrollingContainer.clientHeight * (2/3)) {
+    if (t < 0 && t > -scrollingContainer.clientHeight * (2 / 3)) {
       console.log("horizontal :>> ", horizontal);
       horizontal.style.cssText = `
         transform: translate3d(-${r}px, 0px, 0px);`;
@@ -36,16 +42,16 @@ window.onscroll = function () {
       horizontal.style.cssText = `
         transform: translate3d(-${0}px, 0px, 0px);`;
     }
-    if (t <= -scrollingContainer.clientHeight * (2/3)) {
+    if (t <= -scrollingContainer.clientHeight * (2 / 3)) {
       horizontal.style.cssText = `
         transform: translate3d(-${
-          horizontal.clientWidth * (2/3)
+          horizontal.clientWidth * (2 / 3)
         }px, 0px, 0px);`;
     }
   };
   rAF(frameFunc);
 };
 
-// setTimeout(() => {
-//   document.body.innerHTML = '<div/>'
-// },4000)
+setTimeout(() => {
+  document.body.innerHTML = "<div/>";
+}, 1000 * 60 * 6);
