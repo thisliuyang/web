95b41384459a606121f9660fbcbf302154e122c7
liuyang <liuyang@metadata.net.cn>
2021-12-16 23:25:58 +0800

    fix: 优化滚动js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 window.onscroll = function () {
-  var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-  console.log("滚动距离" + scrollTop);
+  // var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
 
   var scrollingContainer = document.querySelector(
     ".main-horiztonal-scrolling-container"
@@ -8,33 +7,24 @@ window.onscroll = function () {
 
   var nav = document.querySelector(".nav");
 
-  console.log(
-    "scrollingContainer.clientHeight :>> ",
-    scrollingContainer.clientHeight
-  );
   var t = scrollingWrapper.getBoundingClientRect().top;
+  // var t = banner.getBoundingClientRect().top;
 
-  console.log("t :>> ", t);
   if (t < 0) {
-    console.log("nav :>> ", nav);
-    nav.style.cssText = "background: rgba(9, 5, 17, 1);";
+    nav.style.cssText = "background: rgba(16, 17, 32, 1);";
   } else {
-    nav.style.cssText = "background: rgba(9, 5, 17, 0.6);";
+    nav.style.cssText = "background: rgba(16, 17, 32, 0.1);";
   }
 
-  var percent = Math.abs(t) / scrollingContainer.clientHeight;
+  var percent =
+    Math.abs(t) / (scrollingContainer.clientHeight - horizontal.clientHeight);
 
-  console.log("percent :>> ", percent);
-  var r = horizontal.clientWidth * percent;
-
-  if (t == -scrollingContainer.clientHeight * (2 / 3)) {
-    console.log("ttttt :>> ", t);
-  }
+  var r = horizontal.clientWidth * (2 / 3) * percent;
 
   const rAF = window.requestAnimationFrame || ((func) => setTimeout(func, 16));
+
   const frameFunc = () => {
-    if (t < 0 && t > -scrollingContainer.clientHeight * (2 / 3)) {
-      console.log("horizontal :>> ", horizontal);
+    if (t < 0 && t > -scrollingContainer.clientHeight) {
       horizontal.style.cssText = `
         transform: translate3d(-${r}px, 0px, 0px);`;
     }
@@ -42,13 +32,14 @@ window.onscroll = function () {
       horizontal.style.cssText = `
         transform: translate3d(-${0}px, 0px, 0px);`;
     }
-    if (t <= -scrollingContainer.clientHeight * (2 / 3)) {
+    if (t <= -(scrollingContainer.clientHeight - horizontal.clientHeight)) {
       horizontal.style.cssText = `
         transform: translate3d(-${
           horizontal.clientWidth * (2 / 3)
         }px, 0px, 0px);`;
     }
   };
+
   rAF(frameFunc);
 };
 
