ba4c9ea8a4788c849743371e5d10c64c8d05b8d9
liuyang <liuyang@metadata.net.cn>
2022-06-13 22:29:02 +0800

    feat: 优化导航栏

--- js/main.js
+++ js/main.js
@@ -110,3 +110,39 @@ addEvent(1);
 addEvent(2);
 addEvent(3);
 addEvent(4);
+
+var $c = function (array) {
+  var nArray = [];
+  for (var i = 0; i < array.length; i++) nArray.push(array[i]);
+  return nArray;
+};
+Array.prototype.each = function (func) {
+  for (var i = 0, l = this.length; i < l; i++) {
+    func(this[i], i);
+  }
+};
+document.getElementsByClassName = function (cn) {
+  var hasClass = function (w, Name) {
+    var hasClass = false;
+    w.className.split(" ").each(function (s) {
+      if (s == Name) hasClass = true;
+    });
+    return hasClass;
+  };
+  var elems = document.getElementsByTagName("*") || document.all;
+  var elemList = [];
+  $c(elems).each(function (e) {
+    if (hasClass(e, cn)) {
+      elemList.push(e);
+    }
+  });
+  return $c(elemList);
+};
+function change_bg(obj) {
+  console.log('obj :>> ', obj);
+  var a = document.getElementsByClassName("menu")[0].getElementsByTagName("a");
+  for (var i = 0; i < a.length; i++) {
+    a[i].className = "";
+  }
+  obj.className = "active";
+}
